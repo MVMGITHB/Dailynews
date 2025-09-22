120c24b35d73378a450ceb8673624b5b79f2c269
tejaswimvm <tejaswi.mvmbs@gmail.com>
2025-09-22 13:42:03 +0530

    Added Popup to Blogs

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { ArticleHome } from "./ArticleHome";
+import EVPopup from "../popup/page";
 const Article = ({ data }) => {
   return (
     <div>
       <ArticleHome data={data} />
+      <EVPopup data={data} />
     </div>
   );
 };

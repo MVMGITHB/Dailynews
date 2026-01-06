4894e296d8eea026ffa8381216e38ffaf68c1593
Devansh Jinraniya <devanshjinraniya41@gmail.com>
2026-01-06 11:28:44 +0530

    hbvgcvhb

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,26 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["api.dailynewzmail.com", "66.116.198.221" , "api.shopsmaart.com" , "localhost"],
+    domains: [
+      "api.dailynewzmail.com",
+      "66.116.198.221",
+      "api.shopsmaart.com",
+      "localhost",
+    ],
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload",
+          },
+        ],
+      },
+    ];
   },
 };
 

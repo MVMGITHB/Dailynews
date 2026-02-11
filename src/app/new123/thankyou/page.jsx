0e9a63c2d485636f9c3a1478d83cbd26cc878da3
Devansh Jinraniya <devanshjinraniya41@gmail.com>
2026-02-11 13:09:46 +0530

    All code updated

--- src/app/new123/thankyou/page.jsx
+++ src/app/new123/thankyou/page.jsx
@@ -1,12 +1,13 @@
 import ArabicFooter from '@/components/arabicHostpital/ArabicFooter'
 import ArabicHero from '@/components/arabicHostpital/ArabicHero'
+import ArabicNavbar from '@/components/arabicHostpital/ArabicNavbar'
 import ArabicThankYou from '@/components/arabicHostpital/ArabicThankYou'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
-        
+        <ArabicNavbar/>
         <ArabicHero/>
         <ArabicThankYou/>
         <ArabicFooter/>

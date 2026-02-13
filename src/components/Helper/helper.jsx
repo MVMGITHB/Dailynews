db48a8f68a96a1646ae96aef8493cfc001f5539c
Devansh Jinraniya <devanshjinraniya41@gmail.com>
2026-02-13 11:59:59 +0530

    All code updated

--- src/components/Helper/helper.jsx
+++ src/components/Helper/helper.jsx
@@ -2,9 +2,9 @@
 
 //  export const base_url1="https://user.dailynewzmail.com"
 
-//  export const base_url="https://api.dailynewzmail.com"
+ export const base_url="https://api.dailynewzmail.com"
 //  export const base_url=""
 
-export const base_url = "http://localhost:5926";
+// export const base_url = "http://localhost:5926";
 
 export const base_url1 = "https://user.dailynewzmail.com";
